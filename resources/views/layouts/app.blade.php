9e6bcc18e46d4db4826a8f3472e842e515680c1b
Angga Bawole <bawoleangga03@gmail.com>
2021-11-11 02:26:35 +0800

    initial setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,22 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>
-		@yield('title')
-	</title>
+	<title>@yield('title')</title>
+	
+  @stack('prepend-style')
+  @include('includes.style')
+  @livewireStyles
+  @stack('addon-style')
+
 </head>
 <body>
 	
 	@yield('content')
+
+	@stack('prepend-script')
+  @include('includes.script')
+  @livewireScripts
+  @stack('addon-script')
 
 </body>
 </html>
